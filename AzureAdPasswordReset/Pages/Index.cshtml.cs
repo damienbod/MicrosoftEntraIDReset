c8470785f6c4a0a8d0bce5d38965a15f967d6919
damienbod <damien_bod@hotmail.com>
2023-06-02 19:50:03 +0200

    Init search

--- AzureAdPasswordReset/Pages/Index.cshtml.cs
+++ AzureAdPasswordReset/Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AzureAdPasswordReset.Pages;
 
 public class IndexModel : PageModel
 {
     private AadGraphSdkManagedIdentityAppClient _graphUsers;
+    public string? SearchText { get; set; }
 
     public IndexModel(AadGraphSdkManagedIdentityAppClient graphUsers)
     {
@@ -15,5 +17,23 @@ public class IndexModel : PageModel
         var usersCollectionResponse = await _graphUsers.FindUsers("li");
 
         var users = usersCollectionResponse.Value!.ToList();
+    }
+
+    public async Task<ActionResult> OnGetAutoCompleteSuggest(string term)
+    {
+        var usersCollectionResponse = await _graphUsers.FindUsers(term);
+
+        var users = usersCollectionResponse!.Value!.ToList();
+
+        var usersDisplay = users.Select(user => new
+        {
+            Id = user.Id,
+            Upn = user.UserPrincipalName,
+            DisplayName = user.DisplayName,
+        });
+
+        SearchText = term;
+
+        return new JsonResult(usersDisplay);
     }
 }
